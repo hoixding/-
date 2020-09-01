b7cef29cad4fca112c11034e6cadfa414d4ce167
hoi <hoixding@126.com>
2020-09-01 21:55:19 +0800

    界面重置，结果放大失效，第四次更新核心算法

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,169 +37,177 @@ double MainWindow::op(std::string a)//计算器算法，输入计算字符串，
 
     for (unsigned int  i = 0; i < a.size(); i++) {
         if (a[i] == '-') {
-            if (i == 0) {
-                i++;
-                char tem_num1[250];
-                int j = 0;
-                double tem_num = 0;
-                while ((a[i] >= '0' && a[i] <= '9') || a[i] == '.') {
-                    tem_num1[j] = a[i];
-                    j++;
-                    i++;
-                }
-                tem_num1[j] = '\0';
-                tem_num = atof(tem_num1);
-                my_struct uu;
-                uu.ch = '\0';
-                uu.type = 0;
-                uu.num = -tem_num;
-                p.push(uu);
-                i--;
-            }
-            else if ((a[i - 1] < '0' || a[i - 1] > '9')&&a[i-1]!=')') {
-                i++;
-                char tem_num1[250];
-                int j = 0;
-                double tem_num = 0;
-                while ((a[i] >= '0' && a[i] <= '9' )|| a[i] == '.') {
-                    tem_num1[j] = a[i];
-                    j++;
-                    i++;
-                }
-                tem_num1[j] = '\0';
-                tem_num = atof(tem_num1);
-                my_struct uu;
-                uu.ch = '\0';
-                uu.type = 0;
-                uu.num = -tem_num;
-                p.push(uu);
-                i--;
-            }
-            else {
-                my_struct uu;
-                uu.type = 1;
-                uu.num = 0;
-                char topch;
-                topch = opera.getTop();
-                if (topch == '*' || topch == '/'||topch == '-') {
-                    opera.pop();
-                    uu.ch = topch;
-                    p.push(uu);
-                    i--;
-                }
-                else {
-                    opera.push(a[i]);
-                }
+             if (i == 0) {
+                 i++;
+                 char tem_num1[250];
+                 int j = 0;
+                 double tem_num = 0;
+                 while ((a[i] >= '0' && a[i] <= '9') || a[i] == '.') {
+                     tem_num1[j] = a[i];
+                     j++;
+                     i++;
+                 }
+                 tem_num1[j] = '\0';
+                 tem_num = atof(tem_num1);
+                 my_struct uu;
+                 uu.ch = '\0';
+                 uu.type = 0;
+                 uu.num = -tem_num;
+                 p.push(uu);
+                 i--;
 
-            }
+             }
+             else if ((a[i - 1] < '0' || a[i - 1] > '9') && a[i - 1] != ')') {
 
-        }
-        else if (a[i] == '+'  || a[i] == '*' || a[i] == '(' || a[i] == ')' || a[i] == '/') {
-            my_struct uu;
-            uu.type = 1;
-            uu.num = 0;
-            char topch;
-            switch (a[i]) {
-                case '(':
-                    opera.push(a[i]);
-                    break;
-                case '+':
-                    if (opera.empty()) {
-                        opera.push(a[i]);
-                    }
-                    else {
-                        topch = opera.getTop();
-                        if (topch == '*' || topch == '/'||topch=='+') {
-                            opera.pop();
-                            uu.ch = topch;
-                            p.push(uu);
-                            i--;
-                        }
-                        else {
-                            opera.push(a[i]);
+                 i++;
+                 char tem_num1[250];
+                 int j = 0;
+                 double tem_num = 0;
+                 while ((a[i] >= '0' && a[i] <= '9') || a[i] == '.') {
+                     tem_num1[j] = a[i];
+                     j++;
+                     i++;
+                 }
+                 tem_num1[j] = '\0';
+                 tem_num = atof(tem_num1);
+                 my_struct uu;
+                 uu.ch = '\0';
+                 uu.type = 0;
+                 uu.num = -tem_num;
+                 p.push(uu);
+                 i--;
+             }
+             else {
+                 my_struct uu;
+                 uu.type = 1;
+                 uu.num = 0;
+                 char topch;
+                 topch = opera.getTop();
+                 if (topch == '*' || topch == '/' || topch == '-'||topch=='+') {
+                     opera.pop();
+                     uu.ch = topch;
+                     p.push(uu);
+                     i--;
+                 }
+                 else {
+                     opera.push(a[i]);
 
-                        }
+                 }
 
-                    }
+             }
 
-                    break;
+         }
 
-                case '*':
-                if (opera.empty()) {
-                    opera.push(a[i]);
-                }
-                else {
-                    topch = opera.getTop();
-                    if (topch == '*' ) {
-                        opera.pop();
-                        uu.ch = topch;
-                        p.push(uu);
-                        i--;
-                    }
-                    else {
-                        opera.push(a[i]);
+         else if (a[i] == '+' || a[i] == '*' || a[i] == '(' || a[i] == ')' || a[i] == '/') {
+             my_struct uu;
+             uu.type = 1;
+             uu.num = 0;
+             char topch;
+             switch (a[i]) {
+             case '(':
+                 opera.push(a[i]);
+                 break;
+             case '+':
+                 if (opera.empty()) {
+                     opera.push(a[i]);
+                 }
+                 else {
+                     topch = opera.getTop();
+                     if (topch == '*' || topch == '/' || topch == '+'||topch=='-') {
+                         opera.pop();
+                         uu.ch = topch;
+                         p.push(uu);
+                         i--;
+                     }
+                     else {
+                         opera.push(a[i]);
 
-                    }
+                     }
 
-                }
+                 }
 
-                    break;
-                case '/':
-                if (opera.empty()) {
-                    opera.push(a[i]);
-                }
-                else {
-                    topch = opera.getTop();
-                    if (topch == '/' ) {
-                        opera.pop();
-                        uu.ch = topch;
-                        p.push(uu);
-                        i--;
-                    }
-                    else {
-                        opera.push(a[i]);
+                 break;
 
-                    }
+             case '*':
 
-                }
-                    break;
-                case ')':
-                    {
-                        while (opera.getTop() != '(') {
-                            my_struct o;
-                            o.type = 1;
-                            o.num = 0;
-                            o.ch = opera.getTop();
-                            opera.pop();
-                            p.push(o);
-                        }
-                        opera.pop();
-                    }
 
-                    break;
-            }
+                 if (opera.empty()) {
+                     opera.push(a[i]);
+                 }
+                 else {
+                     topch = opera.getTop();
+                     if (topch == '*'||topch=='/') {
+                         opera.pop();
+                         uu.ch = topch;
+                         p.push(uu);
+                         i--;
+                     }
+                     else {
+                         opera.push(a[i]);
 
-        }
-        else {
-            char tem_num1[250];
-            int j = 0;
-            double tem_num = 0;
-            while ((a[i] >= '0' && a[i] <= '9' )|| a[i] == '.') {
-                tem_num1[j] = a[i];
-                j++;
-                i++;
-            }
-            tem_num1[j] = '\0';
-            tem_num = atof(tem_num1);
-            my_struct uu;
-            uu.ch = '\0';
-            uu.type = 0;
-            uu.num = tem_num;
-            p.push(uu);
-            i--;
-        }
+                     }
 
-    }
+                 }
+
+                 break;
+             case '/':
+                 if (opera.empty()) {
+                     opera.push(a[i]);
+                 }
+                 else {
+                     topch = opera.getTop();
+                     if (topch == '/'||topch=='*') {
+                         opera.pop();
+                         uu.ch = topch;
+                         p.push(uu);
+                         i--;
+                     }
+                     else {
+                         opera.push(a[i]);
+
+                     }
+
+                 }
+                 break;
+             case ')':
+             {
+                 while (opera.getTop() != '(') {
+                     my_struct o;
+                     o.type = 1;
+                     o.num = 0;
+                     o.ch = opera.getTop();
+                     opera.pop();
+                     p.push(o);
+                 }
+                 opera.pop();
+             }
+
+             break;
+             }
+
+
+         }
+         else {
+             char tem_num1[250];
+             int j = 0;
+             double tem_num = 0;
+             while ((a[i] >= '0' && a[i] <= '9') || a[i] == '.') {
+                 tem_num1[j] = a[i];
+                 j++;
+                 i++;
+             }
+             tem_num1[j] = '\0';
+             tem_num = atof(tem_num1);
+             my_struct uu;
+             uu.ch = '\0';
+             uu.type = 0;
+             uu.num = tem_num;
+             p.push(uu);
+             i--;
+         }
+
+
+     }
     while (opera.empty() != 1) {
         my_struct uu;
         uu.type = 1;
