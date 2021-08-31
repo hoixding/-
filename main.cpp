9f8ec542210828f19b8ce080d0f927dd539ed370
白丰 <32367628+hoixding@users.noreply.github.com>
2021-08-31 09:08:30 +0800

    最终注释，停止更新，bug已修复

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-    return a.exec();
+    w.show();//显示窗口
+    return a.exec();//递归
+    //以上为QT的常用main写法
 }
